ebbb5cbb2b024d6a83f1f789fce8d983008c44a3
LabXFrontendDev <93455023+LabXFrontendDev@users.noreply.github.com>
2022-01-25 10:21:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="content">
+        <p>This is my main content</p>
+      </section>
+      <aside className="sidebar">
+        <div className="sidebar--item">
+          <div className="item item-1"></div>
+        </div>
+        <div className="sidebar--item">
+          <div className="item item-2"></div>
+        </div>
+        <div className="sidebar--item">
+          <div className="item item-3"></div>
+        </div>
+        <div className="sidebar--item">
+          <div className="item item-4"></div>
+        </div>
+        <div className="sidebar--item">
+          <div className="item item-5"></div>
+        </div>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
